72a523c6c64b3bae4fa05555db5efad8e150dc12
Raghu Betina <raghu@firstdraft.com>
2018-03-07 15:08:25 +0000

    Direct association between users and bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,8 @@
 class Bookmark < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   belongs_to :dish,
              :class_name => "Reservation",
              :foreign_key => "reservation_id"
