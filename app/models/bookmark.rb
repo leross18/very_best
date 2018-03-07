9cc21101145caa2ecb4b5f29f02fa8345ec83e50
Raghu Betina <raghu@firstdraft.com>
2018-03-07 15:08:25 +0000

    Direct association between reservations and bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,10 @@
 class Bookmark < ApplicationRecord
   # Direct associations
 
+  belongs_to :dish,
+             :class_name => "Reservation",
+             :foreign_key => "reservation_id"
+
   # Indirect associations
 
   # Validations
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,9 @@
 class Reservation < ApplicationRecord
   # Direct associations
 
+  has_many   :bookmarks,
+             :dependent => :destroy
+
   has_many   :venues,
              :class_name => "Restaurant",
              :foreign_key => "dish_id",
